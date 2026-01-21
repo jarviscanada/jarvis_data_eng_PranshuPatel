df780c0a2ef1f3f1fdc77ffaf4836c1ca87d7c7e
Pranshu Patel <pranshu.patel232002@gmail.com>
2026-01-21 18:08:01 +0000

    Add ddl.sql to create host_info and host_usage tables

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,31 @@
+-- ddl.sql
+-- Creates tables for host_agent database
+-- Run using:
+-- psql -h localhost -U postgres -d host_agent -f sql/ddl.sql
+
+CREATE TABLE IF NOT EXISTS public.host_info
+(
+  id               SERIAL PRIMARY KEY,
+  hostname         VARCHAR NOT NULL UNIQUE,
+  cpu_number       SMALLINT NOT NULL,
+  cpu_architecture VARCHAR NOT NULL,
+  cpu_model        VARCHAR NOT NULL,
+  cpu_mhz          DOUBLE PRECISION NOT NULL,
+  l2_cache         INTEGER NOT NULL,
+  total_mem        INTEGER,
+  "timestamp"      TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS public.host_usage
+(
+  "timestamp"    TIMESTAMP NOT NULL,
+  host_id        SERIAL NOT NULL,
+  memory_free    INTEGER NOT NULL,
+  cpu_idle       SMALLINT NOT NULL,
+  cpu_kernel     SMALLINT NOT NULL,
+  disk_io        INTEGER NOT NULL,
+  disk_available INTEGER NOT NULL,
+  CONSTRAINT host_usage_host_info_fk
+    FOREIGN KEY (host_id) REFERENCES public.host_info(id)
+);
+
